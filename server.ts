7fc5f3073146a438d2db999daf54aa091828e6c7
Jo <joely.rooke@gmail.com>
2022-01-25 12:36:54 +0000

    added route handlers for /contacts and /users, tested on postman

--- server.ts
+++ server.ts
@@ -3,16 +3,9 @@ import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 
-config(); //Read .env file lines as though they were env vars.
-
-//Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
-//Do not set the environment variable LOCAL if you want to connect to a heroku DB.
-
-//For the ssl property of the DB connection config, use a value of...
-// false - when connecting to a local DB
-// { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+config();
+const herokuSSLSetting = { rejectUnauthorized: false };
+const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
@@ -21,21 +14,88 @@ const dbConfig = {
 const app = express();
 
 app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
+app.use(cors()); //add CORS support to each following route handler
 
 const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
+  const dbres = await client.query("select * from categories");
   res.json(dbres.rows);
 });
 
+app.get("/users", async (req, res) => {
+  const dbres = await client.query("select * from users");
+  res.json({
+    status: "success",
+    data: dbres.rows,
+  });
+});
+
+app.get("/contacts/stops/:stopId", async (req, res) => {
+  const { stopId } = req.params;
+  const dbres = await client.query(
+    "select name, email from contacts join trip_contacts on trip_contacts.contact = contacts.id where trip_contacts.stop = $1",
+    [stopId]
+  );
+  res.json({
+    status: "success",
+    data: dbres.rows,
+  });
+});
+
+app.get("/contacts/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const dbres = await client.query(
+    "select * from contacts where contacts.contact_of = $1",
+    [userId]
+  );
+  res.json({
+    status: "success",
+    data: dbres.rows,
+  });
+});
+
+app.post("/contacts/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const { contactName, contactEmail } = req.body;
+  const dbres = await client.query(
+    "INSERT INTO contacts (contact_of, name, email, activated) values ($1, $2, $3, true) returning * ",
+    [userId, contactName, contactEmail]
+  );
+  res.json({
+    status: "success",
+    data: dbres.rows,
+  });
+});
+
+app.put("/contacts/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const dbres = await client.query(
+    "UPDATE contacts SET activated = not activated WHERE id = $1 RETURNING *",
+    [contactId]
+  );
+  res.json({
+    status: "success",
+    data: dbres.rows,
+  });
+});
+
+app.delete("/contacts/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const dbres = await client.query("delete from contacts WHERE id = $1", [
+    contactId,
+  ]);
+  res.json({
+    status: "success",
+    data: dbres.rows,
+  });
+});
 
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
